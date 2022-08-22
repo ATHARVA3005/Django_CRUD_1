1d447886a4d29b7719bb2c9c341e0710214a0a2e
ATHARVA3005 <warokaratharva305@gmail.com>
2022-08-23 01:38:08 +0530

    Fixed TypeError: __init__() got an unexpected keyword argument 'attrs'

--- enroll/forms.py
+++ enroll/forms.py
@@ -9,7 +9,7 @@ class StudentRegistration(forms.ModelForm):
         widgets = {
             'name': forms.TextInput(attrs={'class':'form-control'}),
 
-            'email': forms.EmailField(attrs={'class':'form-control'}),
+            'email': forms.EmailInput(attrs={'class':'form-control'}),
 
             'password': forms.PasswordInput(attrs={'class':'form-control'}),
         }
